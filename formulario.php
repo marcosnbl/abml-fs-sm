a6add6b07c04037cd458e3c6beed3991bf714e3f
Marcos Nicolás Benitez Lisoski <64109161+marcosnbl@users.noreply.github.com>
2021-01-04 21:59:29 -0300

    modificacion del color del header

--- formulario.php
+++ formulario.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
   </head>
 
-    <nav class="navbar navbar-dark bg-dark">
+    <nav class="navbar navbar-dark bg-primary">
       <div class="container">
         <a class="navbar-brand" href="index.php">ABML Full Stack Silicon Misiones.</a>
       </div>
